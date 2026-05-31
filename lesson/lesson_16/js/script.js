d28420650fb3a0744b54a9803305dc6dd203d44a
Artem Baidachenko <haykljnoun1@gmail.com>
2026-05-31 17:51:13 +0300

    Add files via upload

--- lesson/lesson_16/js/script.js
+++ lesson/lesson_16/js/script.js
@@ -5,3 +5,14 @@ const menu = document.querySelector(".menu")
 menu.addEventListener("click", () => {
 	menu.toggleAttribute("menu-open")
 })
+
+// burger-menu
+// document.addEventListener('click', documentActions)
+
+// function documentActions(e) {
+// 	const targetElement = e.target
+
+// 	if (targetElement.closest('.icon-menu')) {
+// 		document.documentElement.toggleAttribute('data-menu-open')
+// 	}
+// }
